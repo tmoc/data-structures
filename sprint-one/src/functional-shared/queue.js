38ea2d871d02dec5a86298ec85eb89f1c5769298
Hack Reactor Students <accounts@hackreactor.com>
2014-06-04 20:59:58 -0700

    Completed functional-shared pattern.

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,9 +1,41 @@
 var makeQueue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var someInstance = {};
+  someInstance._storage = {};
+  someInstance._size = 0;
+  someInstance._first = 0;
+
+  _.extend(someInstance, queueMethods);
+
+  return someInstance;
 };
 
 var queueMethods = {};
+
+
+queueMethods.enqueue = function (value) {
+  this._storage[this._first + this._size] = value;
+  this._size += 1;
+};
+
+queueMethods.dequeue = function () {
+  var temp;
+
+  if (this._size > 0) {
+    this._size -= 1;
+  }
+
+  temp = this._storage[this._first];
+  delete this._storage[this._first];
+  this._first += 1;
+  return temp;
+};
+
+queueMethods.size = function () {
+  return this._size;
+};
 
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,33 @@
 var makeStack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+
+  someInstance.storage = {};
+  someInstance.storageSize = 0;
+
+  return _.extend(someInstance, stackMethods);
+
 };
 
-var stackMethods = {};
+stackMethods = {};
+
+stackMethods.push = function(value) {
+  this.storage[this.storageSize] = value;
+  this.storageSize++;
+};
+stackMethods.pop = function() {
+  if (this.storageSize > 0) {
+    this.storageSize--;
+  }
+  var temp = this.storage[this.storageSize];
+  delete this.storage[this.storageSize];
+  return temp;
+};
+stackMethods.size = function() {
+  return this.storageSize;
+};
 
 
+// var newStack = );
+// newStack // Object
