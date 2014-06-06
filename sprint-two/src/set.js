ddc276c0d49033b5f26f1368b216df33821f01bd
Hack Reactor Students <accounts@hackreactor.com>
2014-06-05 20:23:34 -0700

    Complete set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,26 @@
 var makeSet = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  this._storage[item] = item;
+
 };
 
 setPrototype.contains = function(item){
+  return this._storage.hasOwnProperty(item);
 };
 
 setPrototype.remove = function(item){
+  delete this._storage[item];
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * Constant
  */
+
